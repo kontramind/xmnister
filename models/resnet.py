e44f8aeb0cfa7b6e688c22337d79b3bc7775b16e
Aleksandar Babic <aleksandar.babic@zivid.com>
2020-05-27 13:44:56 +0200

    Add Kaiming weight initialization

--- models/resnet.py
+++ models/resnet.py
@@ -3,16 +3,26 @@ from functools import partial
 import torch.nn as nn
 from torchvision.models import resnet18, resnet34
 
+def weights_init(model):
+    if isinstance(model, nn.Conv2d) or isinstance(model, nn.Linear):
+        nn.init.kaiming_uniform_(model.weight)
+        if model.bias is not None:
+            nn.init.zeros_(model.bias)
+
 
 def resnet_finetune(model, n_classes):
     """
     This function prepares resnet to be finetuned by:
     1) cut-off the last layer
-    2) replace the last layer with a new one with the correct classes number
+    2) insert new last layer with the correct classes number
     """
     for param in model.parameters():
         param.requires_grad = True
+
     model.fc = nn.Linear(512, n_classes)
+
+    model.apply(weights_init)
+
     return model
 
 
