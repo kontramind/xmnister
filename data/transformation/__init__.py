19704da9ac56162f138d5feaaca399a7504dd8d8
Aleksandar Babic <aleksandar.babic@zivid.com>
2020-05-27 12:46:41 +0200

    Remove augmentation(s) not applicable to EMNIST dataset

--- data/transformation/__init__.py
+++ data/transformation/__init__.py
@@ -11,21 +11,21 @@ class ImgAugTransform:
     def __init__(self):
         self.aug = iaa.Sequential([
             iaa.Sometimes(0.2, iaa.GaussianBlur(sigma=(0, 3.0))),
-            iaa.Sometimes(0.8, iaa.Sequential([
-                iaa.Fliplr(0.5),
-                iaa.Flipud(0.5)
-            ])),
-            iaa.Sometimes(0.5, iaa.Sequential([
-                iaa.Crop(percent=(0.1, 0.2))
-            ])),
-            iaa.LinearContrast((0.75, 1.5)),
-            iaa.Affine(rotate=(-20, 20), mode='symmetric'),
+            # iaa.Sometimes(0.8, iaa.Sequential([
+            #     iaa.Fliplr(0.5),
+            #     iaa.Flipud(0.5)
+            # ])),
+            # iaa.Sometimes(0.5, iaa.Sequential([
+            #     iaa.Crop(percent=(0.1, 0.2))
+            # ])),
+            #iaa.LinearContrast((0.75, 1.5)),
+            iaa.Affine(rotate=(-5, 5), mode='symmetric'),
             iaa.Sometimes(0.8,
                           iaa.Affine(
-                              scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
-                              translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
-                              rotate=(-25, 25),
-                              shear=(-8, 8)
+                            #   scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+                              translate_percent={"x": (-0.05, 0.05), "y": (-0.05, 0.05)},
+                            #   rotate=(-5, 5),
+                            #   shear=(-5, 5)
                           )),
             iaa.AddToHueAndSaturation(value=(-10, 10), per_channel=True)
         ])
