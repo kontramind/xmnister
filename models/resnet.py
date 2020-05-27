6d70737e71e7b2e7b83892d82b67971701a03454
Aleksandar Babic <aleksandar.babic@zivid.com>
2020-05-27 12:26:58 +0200

    Add ResNet34 model

--- models/resnet.py
+++ models/resnet.py
@@ -1,18 +1,24 @@
-import torch.nn as nn
-from torchvision.models import resnet18
 from functools import partial
+
+import torch.nn as nn
+from torchvision.models import resnet18, resnet34
 
 
 def resnet_finetune(model, n_classes):
     """
     This function prepares resnet to be finetuned by:
-    1) freeze the model weights
-    2) cut-off the last layer and replace with a new one with the correct classes number
+    1) cut-off the last layer
+    2) replace the last layer with a new one with the correct classes number
     """
     for param in model.parameters():
-        param.requires_grad = False
+        param.requires_grad = True
     model.fc = nn.Linear(512, n_classes)
     return model
 
+
 # replace the resnet18 function
 resnet18 = partial(resnet_finetune, resnet18(pretrained=True))
+
+
+# replace the resnet34 function
+resnet34 = partial(resnet_finetune, resnet34(pretrained=True))
